1dba4d5e2d50508527d1951ea85ca1f516ebc33e
25JdS01-Josedeleon <jleondb25@gmail.com>
2026-04-07 10:23:28 -0600

    Add files via upload

---
+++ dias_de_la_semana.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("dias de la semana");
+        int dia;
+        dia = Convert.ToInt32(Console.ReadLine());
+
+        switch (dia)
+        {
+            case 1:
+                Console.WriteLine("Lunes");
+                break;
+            case 2:
+                Console.WriteLine("Martes");
+                break;
+            case 3:
+                Console.WriteLine("Miercoles");
+                break;
+            case 4:
+                Console.WriteLine("Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Viernes");
+                break;
+            case 6:
+                Console.WriteLine("Sabado");
+                break;
+            case 7:
+                Console.WriteLine("domingo");
+                break;
+            default:
+                Console.WriteLine("digite un numero entre el 1 al 7");
+                break;
+        }
+    }
+}
